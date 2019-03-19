93b54e79023b2dfa914c8df972dcbc233c2c9c89
Kallem <S531438@nwmissouri.edu>
2019-03-18 23:17:06 -0500

    commiting java file for favorites

---
+++ app/src/main/java/com/example/moviereview/FifthActivity.java
@@ -0,0 +1,15 @@
+package com.example.moviereview;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FifthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fifth);
+        Intent ini = getIntent();
+    }
+}
